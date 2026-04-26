90f9471b1ef177aaddf73744096739908bc81a52
mikolajroot <mikolaj150878@gmail.com>
2026-04-26 17:29:49 +0200

    add routes for heroes with status and power retrieval

---
+++ internal/layers/routes/routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"geo-project/controllers"
+	"geo-project/services"
+
+	"github.com/doug-martin/goqu/v9"
+	"github.com/labstack/echo/v5"
+)
+
+func RegisterLayersRoutes(api *echo.Group, queries *goqu.Database) {
+
+	heroService := services.NewHeroesService(queries)
+	heroController := controllers.NewHeroesController(heroService)
+
+	api.GET("/heroes", heroController.GetHeroesByStatusAndPower)
+
+}
