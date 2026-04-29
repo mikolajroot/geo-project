860094c7364ee43d84768a6914bf384934f73651
mikolajroot <mikolaj150878@gmail.com>
2026-04-29 10:00:31 +0200

    small fix

--- internal/layers/repository/repo_layers.go
+++ internal/layers/repository/repo_layers.go
@@ -64,9 +64,9 @@ func (r *layerRepository) GetLayers(ctx context.Context, status *string, geometr
 		goqu.L("COUNT(*) OVER()").As("total_count"),
 	)
 	if sortBy != nil {
-		query = query.Order(goqu.I(*sortBy).Asc())
+		query = query.Order(goqu.I(*sortBy).Desc())
 	} else {
-		query = query.Order(goqu.I("updated_at").Asc())
+		query = query.Order(goqu.I("updated_at").Desc())
 	}
 
 	query = query.Limit(uint(pageSize)).Offset(uint(offset))
